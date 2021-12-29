2ad7f2ed9ce7d4eb25253f062032cbeef59eb03d
manucarenjot <carenjotje@gmail.com>
2021-12-29 15:45:05 +0100

    Playlist

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,16 @@
 let nomPlaylist =$('#nomPlaylist');
-let lien =$ ('#lien')
+let lien = document.querySelector('#lien');
+let add =$('#add');
+let divPlaylist =$('#playlists');
+
+let arrayPlaylist = [];
+
+
+function addLink() {
+    divPlaylist.html('<a href="'+ lien.value+ '">lien</a>');
+}
+
+add.click(function () {
+    console.log('salut')
+    addLink()
+})
