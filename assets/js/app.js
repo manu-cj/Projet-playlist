37921aa3a523cb2472b67f01767d54fd1834e646
manucarenjot <carenjotje@gmail.com>
2021-12-29 17:39:36 +0100

    Playlist

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,37 @@
-let nomPlaylist =$('#nomPlaylist');
+let nomPlaylist =document.querySelector('#nomPlaylist');
 let lien = document.querySelector('#lien');
-let add =$('#add');
-let divPlaylist =$('#playlists');
+let add =$('.add');
+let create=$('#create');
+let divPlaylist =$('#playlists').clone();
+let body = $('body');
+let clone =$('#clone');
+numéro = 0;
 
 let arrayPlaylist = [];
 
+function createPlaylist() {
+
+    body.append('<div id=maDiv class=classe>'+ '<h1>'+ nomPlaylist.value +'</h1>' +'</div>');
+    body.append('<button class=add>'+ '<h1>ADD</h1>' +'</button>');
+}
 
 function addLink() {
-    divPlaylist.html('<a href="'+ lien.value+ '">lien</a>');
+    $(this).parents().append('<a href="'+ lien.value+ '">'+lien.value+'</a>');
 }
+
+function clonage() {
+    $('#scorpion').appendChild(divPlaylist.clone());
+}
+
+create.click(function () {
+    createPlaylist()
+})
 
 add.click(function () {
     console.log('salut')
     addLink()
+})
+
+clone.click(function () {
+clonage()
 })
